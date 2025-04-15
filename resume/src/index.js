f435390df639ddd1b950f60a09058dcd0116f6aa
BagelSeedz <andgmart@gmail.com>
2025-04-15 14:07:02 -0400

    changed BrowserRouter to HashRouter

--- resume/src/index.js
+++ resume/src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Valentine from './pages/Valentine';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<Valentine/>}/>
         <Route path="/valentine" element={<Valentine/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
