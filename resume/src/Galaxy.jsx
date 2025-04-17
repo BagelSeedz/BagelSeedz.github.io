7061d0821501fe8960f2797118bde0ad18cf63b1
BagelSeedz <87871784+BagelSeedz@users.noreply.github.com>
2025-04-17 14:38:05 -0400

    adjusted positioning

--- resume/src/Galaxy.jsx
+++ resume/src/Galaxy.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useState, useEffect } from "react";
 import galaxy from './assets/galaxy.png'
+import python_icon from "./assets/python.png"
 import "./Home.css"
 
 const GALAXY = <img alt='galaxy' src={galaxy} width={835} height={735}></img>
-const SPEED = 0.02
+const SPEED = 0.005
 
 const CircleMover = ({skills = []}) => {
   const buttonRef = useRef(null);
@@ -28,27 +29,23 @@ const CircleMover = ({skills = []}) => {
   return (
     <div
       style={{
-        position: 'relative',
-        width: '100vw',
-        height: '100vh',
-        top: '50%',   /* center vertically */
-        left: '50%',  /* center horizontally */
-        transform: 'translate(-50%, -100%)',
         pointerEvents: "none"
       }}
     >
         {skills.map((text, index) => (
             <button
-            key={index}
-            ref={buttonRef}
-            style={{
-              position: 'absolute',
-              left: `calc(50% + ${radiusX * Math.cos(angle + ((index+1) * 6/length))}px)`,
-              top: `calc(50% + ${radiusY * Math.sin(angle + ((index+1) * 6/length))}px)`,
-              transform: 'translate(-50%, -50%)',
-            }}
-          >
-            {text}
+              key={index}
+              ref={buttonRef}
+              style={{
+                position: 'absolute',
+                left: `calc(50% + ${radiusX * Math.cos(angle + ((index+1) * 6.2/length))}px)`,
+                top: `calc(50% + ${radiusY * Math.sin(angle + ((index+1) * 6.2/length))}px)`,
+                transform: 'rotate(22deg)',
+                background: 'none',
+                border: 'none'
+              }}
+            >
+              <img alt='python_icon' src={python_icon} width={50} height={50}/>
           </button>
         ))}
     </div>
@@ -61,16 +58,19 @@ class Galaxy extends React.Component {
             <>
                 <div className="galaxy">
                     {GALAXY}
+                    <div className="skills">
+                      <CircleMover skills={[
+                        'hi', 
+                        "hi 2",
+                        "hi 3",
+                        "hi 4",
+                        "hi 5",
+                        "hi 6",
+                        "hi 7"
+                      ]}/>
+                    </div>
                 </div>
-                <CircleMover skills={[
-                    'hi', 
-                    "hi 2",
-                    "hi 3",
-                    "hi 4",
-                    "hi 5",
-                    "hi 6",
-                    "hi 7"
-                ]}/>
+                
             </>
         );
     }
