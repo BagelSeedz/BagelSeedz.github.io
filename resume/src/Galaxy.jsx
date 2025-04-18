97254b896c487b6b13628ed137302b957ba63188
BagelSeedz <andgmart@gmail.com>
2025-04-18 15:54:33 -0400

    added java, C++, C

--- resume/src/Galaxy.jsx
+++ resume/src/Galaxy.jsx
@@ -1,6 +1,9 @@
 import React, { useRef, useState, useEffect } from "react";
 import galaxy from './assets/galaxy.png'
 import python_icon from "./assets/python.png"
+import java_icon from './assets/java.jpg'
+import cpp_icon from "./assets/cpp.png"
+import c_icon from "./assets/c.png"
 import "./Home.css"
 
 const GALAXY = <img alt='galaxy' src={galaxy} width={835} height={735}></img>
@@ -32,7 +35,7 @@ const CircleMover = ({skills = []}) => {
         pointerEvents: "none"
       }}
     >
-        {skills.map((text, index) => (
+        {skills.map((data, index) => (
             <button
               key={index}
               ref={buttonRef}
@@ -45,7 +48,7 @@ const CircleMover = ({skills = []}) => {
                 border: 'none'
               }}
             >
-              <img alt='python_icon' src={python_icon} width={50} height={50}/>
+              <img alt={data.name} src={data.image} width={50} height={50}/>
           </button>
         ))}
     </div>
@@ -60,13 +63,26 @@ class Galaxy extends React.Component {
                     {GALAXY}
                     <div className="skills">
                       <CircleMover skills={[
-                        'hi', 
-                        "hi 2",
-                        "hi 3",
-                        "hi 4",
-                        "hi 5",
-                        "hi 6",
-                        "hi 7"
+                        {
+                          name: 'Python',
+                          image: python_icon,
+                          link: 'https://www.python.org/'
+                        },
+                        {
+                          name: 'Java',
+                          image: java_icon,
+                          link: 'https://www.java.com/'
+                        },
+                        {
+                          name: 'C++',
+                          image: cpp_icon,
+                          link: 'https://en.wikipedia.org/wiki/C++'
+                        },
+                        {
+                          name: 'C',
+                          image: c_icon,
+                          link: 'https://en.wikipedia.org/wiki/C_(programming_language)'
+                        },
                       ]}/>
                     </div>
                 </div>
